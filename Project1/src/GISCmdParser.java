07e3b32ecec2ab9a192036b8dd080727d3474fb0
grubb2k7 <grubb2k7@vt.edu>
2015-01-25 20:23:29 -0500

    added more functions to cmdparser

--- Project1/src/GISCmdParser.java
+++ Project1/src/GISCmdParser.java
@@ -1,17 +1,18 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.FileReader;
+import java.io.RandomAccessFile;
+import java.io.FileWriter;
 import java.io.File;
 
 public class GISCmdParser {
 
-	private FileReader frStream;
+	private RandomAccessFile cmdStream;
+	private FileWriter logStream;
 	private GISParser gisParser;
-	private String logName;
 	
-	public GISCmdParser(File cmdFile, File gisFile, String logFileName) {
+	public GISCmdParser(File cmdFile, File gisFile, File logFile) {
 		try {
-			frStream = new FileReader(cmdFile);
+			cmdStream = new RandomAccessFile(cmdFile, "r");
 		} catch(FileNotFoundException e) {
 			System.err.println("Could not find file " + cmdFile.getName());
 			System.exit(1);
@@ -20,10 +21,39 @@ public class GISCmdParser {
 			System.exit(1);
 		}
 		
+		try {
+			logStream = new FileWriter(logFile);
+		} catch(FileNotFoundException e) {
+			System.err.println("Could not find file " + logFile.getName());
+			System.exit(1);
+		} catch(IOException e) {
+			System.err.println("IO error occured");
+			System.exit(1);
+		}
+		
 		gisParser = new GISParser(gisFile);
-		logName = logFileName;
+		
+		readAndExecut();
 	}
 	
-	private readAndExecute
+	private void readAndExecute() {
+		String line = null;
+		
+		try {
+			while((line = cmdStream.readLine()) != null) {
+				if(line.contains(";")) {
+					continue;
+				}
+				
+			}
+		} catch(IOException e) {
+			System.err.println("IO error occured");
+			System.exit(1);
+		}
+	}
+	
+	private parseCmd(String cmd) {
+		
+	}
 
 }
